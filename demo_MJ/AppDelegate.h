fa84e385dcbd8a11c8333a9a7475353ff04dea2d
fangtang0101 <623003648@qq.com>
2015-08-17 21:55:33 +0800

    Initial Commit

---
+++ demo_MJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo_MJ
+//
+//  Created by 春高方 on 15/8/17.
+//  Copyright (c) 2015年 春高方. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
